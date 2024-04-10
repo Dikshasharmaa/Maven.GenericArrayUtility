97618b4576485f46e734588db43b2d684271f0f3
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-04-09 23:32:42 -0400

    Almost done

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,39 +1,73 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<T>{
-    public T[] inputArray;
+    private T[] inputArray;
     public ArrayUtility(T[] inputArray){
         this.inputArray = inputArray;
     }
 
     public T[] removeValue(T valueToRemove) {
-        T[] array = (T[]) new Object[inputArray.length];
-        for(int i =0; i< inputArray.length; i++){
-            if(inputArray[i] != valueToRemove){
-                array[i] = inputArray[i];
+        List<T> array = new ArrayList<>();
+        for(T t : inputArray){
+            if(!t.equals(valueToRemove)){
+                array.add(t);
             }
         }
 
-
-        return array;
+        return array.toArray(Arrays.copyOf(inputArray,array.size()));
     }
 
-    public T getNumberOfOccurrences(T valueToEvaluate) {
-        T count = null;
+    public int getNumberOfOccurrences(T valueToEvaluate) {
+        int count = 0;
         int sum =0;
         for(int i =0 ; i< inputArray.length;i++){
-            if(inputArray[i] == valueToEvaluate){
-                count = sum+1;
+            if(inputArray[i].equals(valueToEvaluate)){
+                count++;
             }
         }
 
         return count;
     }
 
-    public T countDuplicatesInMerge(Integer[] arrayToMerge, T valueToEvaluate) {
-        return null;
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        //T mergedArray[] = (T[]) new Object[inputArray.length + arrayToMerge.length];
+        int count =0;
+        for(int i = 0; i<inputArray.length;i++){
+            if(inputArray[i].equals(valueToEvaluate)){
+                count++;
+            }
+        }
+        for(int j =0;j<arrayToMerge.length;j++){
+            if(arrayToMerge[j].equals(valueToEvaluate)){
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        int maxCount =0;
+        T value = null;
+        for(int i =0; i<inputArray.length;i++){
+            int count =0;
+            for(int j =0;j<= inputArray.length;j++){
+                if(inputArray[i] == inputArray[j]){
+                    count++;
+                }
+            }
+            if(count>maxCount){
+                value = inputArray[i];
+            }
+        }
+
+        return value;
     }
 }
