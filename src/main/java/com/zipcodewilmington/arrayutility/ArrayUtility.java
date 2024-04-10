23bfdede0c29ebaaf64698ac5a9b13a0ce55588b
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-04-09 21:31:14 -0400

    Working

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,37 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T>{
+    public T[] inputArray;
+    public ArrayUtility(T[] inputArray){
+        this.inputArray = inputArray;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        T[] array = (T[]) new Object[inputArray.length];
+        for(int i =0; i< inputArray.length; i++){
+            if(inputArray[i] != valueToRemove){
+                array[i] = inputArray[i];
+            }
+        }
+
+
+        return array;
+    }
+
+    public T getNumberOfOccurrences(T valueToEvaluate) {
+        T count = null;
+        int sum =0;
+        for(int i =0 ; i< inputArray.length;i++){
+            if(inputArray[i] == valueToEvaluate){
+                count = sum+1;
+            }
+        }
+
+        return count;
+    }
+
+    public T countDuplicatesInMerge(Integer[] arrayToMerge, T valueToEvaluate) {
+        return null;
+    }
 }
